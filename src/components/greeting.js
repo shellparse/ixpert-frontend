e7ceceab4e5a7d129f24529015f6146a7b979c82
greekmido <onex911@hotmail.com>
2022-08-20 11:30:48 +0200

    edit logo path to work on heroku

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -4,7 +4,7 @@ export default function Greeting(){
     return(
       <div>
         <h2 className="greetings">Welcome, {Userfront.user.name}</h2>
-        <img src="%PUBLIC_URL%/siteLogo.svg" />
+        <img src="/siteLogo.svg" />
       </div>
     )
 }
