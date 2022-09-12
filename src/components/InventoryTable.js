e5c71f9d0feb8df693cd75fc5757b23be8728146
greekmido <onex911@hotmail.com>
2022-09-12 20:56:46 +0200

    implementing material table with scrolls

--- src/components/InventoryTable.js
+++ src/components/InventoryTable.js
@@ -1,6 +1,16 @@
-import {createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, SortingState, useReactTable} from '@tanstack/react-table'
+import {createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, useReactTable} from '@tanstack/react-table'
 import { useState } from 'react'
 import RowActions from '../components/RowActions'
+import ArrowDownward from '@mui/icons-material/ArrowDownward'
+import ArrowUpward  from '@mui/icons-material/ArrowUpward'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+
 const columnHelper = createColumnHelper()
 const columns = [
     columnHelper.display({
@@ -48,37 +58,35 @@ const columns = [
 
 export default function InventoryTable (props) {
     const [sorting, setSorting] = useState([])
-    const table=useReactTable({data:props.data, columns:columns, state: {sorting}, getCoreRowModel: getCoreRowModel(),getSortedRowModel: getSortedRowModel(), onSortingChange: setSorting,enableRowSelection:true })
+    const table=useReactTable({data:props.data, columns:columns, state: {sorting}, getCoreRowModel: getCoreRowModel(),getSortedRowModel: getSortedRowModel(), onSortingChange: setSorting })
     return (
-        <table>
-            <thead>
-                {table.getHeaderGroups().map((headerGroup)=>{
-                    return <tr key={headerGroup.id}>{headerGroup.headers.map((header)=>{
-                        return <th key={header.id} onClick={header.column.getToggleSortingHandler()} style={header.column.getCanSort()?{cursor:'pointer', userSelect:'none'}:{}}>{
-                            flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}{{
-                                asc: ' 🔼',
-                                desc: ' 🔽',
-                              }[header.column.getIsSorted()] ?? null}</th>
-                    })}</tr>
-                })}
-            </thead>
-            <tbody>
-                {table.getRowModel().rows.map((row)=>{
-                    return <tr style={row.id%2===0?{backgroundColor:'#d7edff'}:{backgroundColor:''}} key={row.id}>{row.getVisibleCells().map((cell)=>{
-                        return <td key={cell.id}>{
-                            flexRender(cell.column.columnDef.cell,
-                                cell.getContext()
-                                )}</td>
-                    })}</tr>
-                })}
-            </tbody>
-            <tfoot>
-               
-            </tfoot>
-        </table>
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth:650}} aria-label='simple table'>
+                <TableHead>
+                    {table.getHeaderGroups().map((headerGroup)=>{
+                        return <TableRow key={headerGroup.id}>{headerGroup.headers.map((header)=>{
+                            return <TableCell key={header.id} onClick={header.column.getToggleSortingHandler()} style={header.column.getCanSort()?{cursor:'pointer', userSelect:'none'}:{}}>{
+                                flexRender(
+                                    header.column.columnDef.header,
+                                    header.getContext()
+                                )}{{
+                                    asc: <ArrowUpward />,
+                                    desc: <ArrowDownward />,
+                                }[header.column.getIsSorted()] ?? null}</TableCell>
+                        })}</TableRow>
+                    })}
+                </TableHead>
+                <TableBody>
+                    {table.getRowModel().rows.map((row)=>{
+                        return <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }} style={row.id%2===0?{backgroundColor:'#d7edff'}:{backgroundColor:''}} key={row.id}>{row.getVisibleCells().map((cell)=>{
+                            return <TableCell key={cell.id}>{
+                                flexRender(cell.column.columnDef.cell,
+                                    cell.getContext()
+                                    )}</TableCell>
+                        })}</TableRow>
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
-    
 }
