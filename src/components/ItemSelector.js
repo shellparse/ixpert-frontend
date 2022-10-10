ba1a30f74198c030fa4d815b07b68ae466b34495
mohammad alameri <onex911@hotmail.com>
2022-10-10 16:00:03 +0200

    not much

--- src/components/ItemSelector.js
+++ src/components/ItemSelector.js
@@ -5,7 +5,7 @@ const filter = createFilterOptions()
 export default function ItemSelector () {
     const [items, setItems] = useState([])
     const [itemValue, setItemValue] = useState(null)
-    //const [skuEntry, setSkuEntry] = useState('')
+    const [emptySearch, setEmptySearch] = useState(false)
     return(
 
     <Autocomplete
@@ -19,22 +19,18 @@ export default function ItemSelector () {
       const filtered = filter(options, params);
       const { inputValue } = params;
       // search the database 
-      const isExisting = options.some((option) => option.sku.includes(inputValue));
-      console.log('is existing', isExisting)
-      if (inputValue !== '' && !isExisting) {
-        console.log('getting new values')
+      
+      const isExisting = options.some((option) => option.sku.toLowerCase().includes(inputValue.toLowerCase()));
+      if (inputValue !== '' && !isExisting && !emptySearch) {
         fetch(`${process.env.REACT_APP_API_URI}/inventory?sku=${inputValue}`)
         .then(response=>response.json())
         .then((data)=>{
-          console.log('setting items')
+          if (data.length===0){
+            setEmptySearch(true)
+          }
           setItems(data)
         })
-        // filtered.push({
-        //   inputValue,
-        //   sku: `search "${inputValue}"`,
-        // });
       }
-
       return filtered;
     }}
     renderInput={(params) =><TextField {...params} varian={'standard'} label={"add item"}/>}
