911911304ce03c32a5eb70f70d1bc88050b62f6d
mohammad alameri <onex911@hotmail.com>
2022-10-26 16:23:35 +0200

    revamping main nav

--- src/components/dashComponents/MainNav.js
+++ src/components/dashComponents/MainNav.js
@@ -1,17 +1,45 @@
-import { Drawer } from "@mui/material"
+import PeopleAltTwoToneIcon from '@mui/icons-material/PeopleAltTwoTone'
+import EngineeringTwoToneIcon from '@mui/icons-material/EngineeringTwoTone'
+import Inventory2TwoToneIcon from '@mui/icons-material/Inventory2TwoTone'
+import DescriptionTwoToneIcon from '@mui/icons-material/DescriptionTwoTone';
 import { NavLink } from "react-router-dom"
-
+import { IconButton, Paper } from '@mui/material';
 export default function MainNav(){
     return(
-        <div className={'dashMainNav'} >
-            <Drawer variant="permanent" >
-                <ul style={{listStyle:'none'}}>
-                    <li><NavLink className={({isActive}) => isActive?"activeNav":""} to={"customers"}>customers</NavLink></li>
-                    <li><NavLink className={({isActive}) => isActive?"activeNav":""} to={"repairslip"}>repair slip</NavLink></li>
-                    <li><NavLink className={({isActive}) => isActive?"activeNav":""} to={"inventory"}>inventory</NavLink></li>
-                    <li><NavLink className={({isActive}) => isActive?"activeNav":""} to={"invoice"}>invoice</NavLink></li>
+            <Paper variant='outlined' className={'dashMainNav'}>
+                <ul>
+                    {[{
+                        label: 'Customers',
+                        link: 'customers',
+                        icon: PeopleAltTwoToneIcon
+                    },{
+                        label: 'Repair slip',
+                        link: 'repairslip',
+                        icon: EngineeringTwoToneIcon
+                    },{
+                        label: 'Inventory',
+                        link: 'inventory',
+                        icon: Inventory2TwoToneIcon
+                    },{
+                        label: 'Invoice',
+                        link: 'invoice',
+                        icon: DescriptionTwoToneIcon
+                    }].map((element, index) => {
+                        return (<li key={index+1}>
+                                    <NavLink children={({isActive})=>isActive?<IconButton  sx={{borderRadius:0, width:'100%', justifyContent:'left', color:'secondary.main'}}>
+                                            <element.icon/>
+                                            {element.label}
+                                        </IconButton>:<IconButton  sx={{borderRadius:0, width:'100%', justifyContent:'left', color:'primary.main'}}>
+                                            <element.icon/>
+                                            {element.label}
+                                        </IconButton>} className={({isActive})=> isActive?'activeNav':'notActiveNav'} to={element.link}>
+                                    </NavLink>
+                                </li>)
+                            }
+                        )}
                 </ul>
-            </Drawer>
-            </div>
+                </Paper>
+ 
+            
     )
 }
