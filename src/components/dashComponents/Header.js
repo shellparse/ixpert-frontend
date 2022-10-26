97086bd80104bf184995028f615345f6e33228ad
greekmido <onex911@hotmail.com>
2022-10-26 20:04:06 +0200

    something

--- src/components/dashComponents/Header.js
+++ src/components/dashComponents/Header.js
@@ -1,10 +1,13 @@
 import Greeting from "../greeting"
 import LogOut from "../../routes/logout"
+import { Box } from "@mui/material"
 export default function Header(){
     return(
-        <div className="dashHeader">
+        // <div className="dashHeader">
+            <Box className="dashHeader" sx={{padding:3, backgroundColor: 'primary.main'}}>
                 <Greeting className="greetings"/>
                 <div className="logout"><LogOut /></div>
-        </div>
+                </Box>
+        // </div>
     )
 }
