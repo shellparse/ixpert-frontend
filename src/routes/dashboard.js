4999ecc6c3a97684940b80d59d5366e58ebe97b3
mohammad alameri <onex911@hotmail.com>
2022-10-31 16:27:39 +0200

    layout

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,9 +3,8 @@ import Footer from '../components/dashComponents/Footer'
 import Header from '../components/dashComponents/Header'
 import Main from '../components/dashComponents/Main'
 import MainNav from '../components/dashComponents/MainNav'
-import SecondaryNav from '../components/dashComponents/SecondaryNav'
 import { useState } from 'react'
-import { Container, Box } from '@mui/system'
+import { Container } from '@mui/system'
 
 export default function DashBoard() {
     const [activeCustomer, setActiveCustomer] = useState({})
@@ -19,8 +18,8 @@ export default function DashBoard() {
             <Header />
             <MainNav />
             <Container sx={{width: `calc(100% - 240px)`, marginLeft: '240px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: `calc(100% - 72px)`}}>
-            <Main activeCustomer={activeCustomer} setCustomer={setActiveCustomer} invoiceFooter={invoiceFooter} setInvoiceFooter={setInvoiceFooter} invoiceItems={invoiceItems} setInvoiceItems={setInvoiceItems} invoiceNumber={invoiceNumber} setInvoiceNumber={setInvoiceNumber} snackBarMsg={snackBarMsg} setSnackBarMsg={setSnackBarMsg} /> 
-            <Footer invoiceFooter={invoiceFooter} setInvoiceFooter={setInvoiceFooter} invoiceItems={invoiceItems} setInvoiceItems={setInvoiceItems} setInvoiceNumber={setInvoiceNumber} snackBarMsg={snackBarMsg} setSnackBarMsg={setSnackBarMsg} />
+                <Main activeCustomer={activeCustomer} setCustomer={setActiveCustomer} invoiceFooter={invoiceFooter} setInvoiceFooter={setInvoiceFooter} invoiceItems={invoiceItems} setInvoiceItems={setInvoiceItems} invoiceNumber={invoiceNumber} setInvoiceNumber={setInvoiceNumber} snackBarMsg={snackBarMsg} setSnackBarMsg={setSnackBarMsg} /> 
+                <Footer invoiceFooter={invoiceFooter} setInvoiceFooter={setInvoiceFooter} invoiceItems={invoiceItems} setInvoiceItems={setInvoiceItems} setInvoiceNumber={setInvoiceNumber} snackBarMsg={snackBarMsg} setSnackBarMsg={setSnackBarMsg} />
             </Container>
         </div>
     )
