2c14225ba642e45b56bbb37829d98796833c5b08
greekmido <onex911@hotmail.com>
2022-11-11 22:59:39 +0200

    create repair slip

--- src/components/CreateRepairSlip.js
+++ src/components/CreateRepairSlip.js
@@ -4,9 +4,9 @@ import { Grid, Box, Typography, Divider, FormGroup, TextField, Button, FormContr
 import { DataGrid } from '@mui/x-data-grid'
 import CustomerSelector from './CustomerSelector'
 import RepairsToolbar from './RepairsToolbar'
-import { ImageNotSupportedSharp } from '@mui/icons-material'
 
 export default function CreateRepairSlip({ setSnackBarMsg }) {
+    const [repairsSelection, setRepairsSelection] = useState([])
     const [inputs, setInputs] = useState({
         slipNumber: '',
         imei: '',
@@ -17,13 +17,13 @@ export default function CreateRepairSlip({ setSnackBarMsg }) {
         checkInStat: {
             'frontCamera': false,
             'backCamera': false,
-            'backGlass': false,
             'frontGlass': false,
-            'lcd': false,
+            'backGlass': false,
             'network': false,
+            'lcd': false,
             'chargingPort': false,
-            'battery': false,
             'wirelessCharging': false,
+            'battery': false,
             'fingerPrint': false,
             'faceId': false,
             'speaker': false,
@@ -33,6 +33,7 @@ export default function CreateRepairSlip({ setSnackBarMsg }) {
         neededRepairs: ['glass', 'ass', 'battery'],
         cashier: Userfront.user.name,
         returned: false,
+        notes: ''
     })
 
 
@@ -43,21 +44,22 @@ export default function CreateRepairSlip({ setSnackBarMsg }) {
             flex: 1
         }
     ]
-
+    console.log(inputs.checkInStat)
     const API = process.env.REACT_APP_API_URI
     function handleSubmit(e) {
 
     }
     function handleChange(e) {
+        console.log(e.target.value)
         let target = e.target
         let value = target.value
         let name = target.name
         if (name === "repairItem") {
             return
-        } else if (target.type === "checkbox") {
+        } else if (target.type === "checkbox" && name!=='') {
             setInputs((values) => ({ ...values, checkInStat: { ...values.checkInStat, [name]: value === "on" ? true : false } }))
         } else if (name === 'notes') {
-            setInputs((values) => ({ ...values, checkInStat: { ...values.checkInStat, [name]: value } }))
+            setInputs((values) => ({ ...values, [name]: value }))
         } else {
             if (name === "total") value = parseFloat(value)
             setInputs((values) => ({ ...values, [name]: value }))
@@ -78,29 +80,24 @@ export default function CreateRepairSlip({ setSnackBarMsg }) {
         <Box sx={{ padding: 2 }} component={'form'} onSubmit={handleSubmit} onChange={handleChange}>
             <Typography variant={'h4'} >Create repair slip</Typography>
             <Divider sx={{ margin: 2 }} />
-            <Grid container spacing={2} >
+            <Grid container spacing={2}  >
                 <Grid xs={4} item>
-                    <TextField name='slipNumber' fullWidth size='small' type={'text'} inputProps={{ readOnly: true }} value={inputs.slipNumber} label={'Slip No'} />
-                </Grid>
-                <Grid xs={4} item>
-                    <CustomerSelector />
-                </Grid>
-                <Grid item xs={4}>
-                    <FormLabel>Phone details:</FormLabel>
+                    <TextField sx={{marginBottom: 2}} name='slipNumber' fullWidth size='small' type={'text'} inputProps={{ readOnly: true }} value={inputs.slipNumber} label={'Slip No'} />
+                    <FormLabel component="legend" >Phone details:</FormLabel>
                     <TextField sx={{ marginTop: 1 }} name='imei' fullWidth size='small' type={'text'} value={inputs.imei} label={'IMEI number'} />
                     <TextField sx={{ marginTop: 1 }} name='brand' fullWidth size='small' type={'text'} value={inputs.brand} label={'Brand'} />
                     <TextField sx={{ marginTop: 1 }} name='model' fullWidth size='small' type={'text'} value={inputs.model} label={'Model'} />
                     <TextField sx={{ marginTop: 1, backgroundColor: `${inputs.color}` }} name='color' fullWidth size='small' type={'text'} value={inputs.color} label={'Color'} />
                     <TextField sx={{ marginTop: 1 }} name='passcode' fullWidth size='small' type={'text'} value={inputs.passcode} label={'Pass code'} />
                 </Grid>
-                <Grid item xs={4}>
-                    <Box sx={{ display: 'flex' }}>
-                        <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-                            <FormLabel component="legend">Phone Status</FormLabel>
-                            <FormGroup>
-                                {Object.keys(inputs.checkInStat).map((state) => {
-                                    console.log(inputs.checkInStat)
-                                    return (
+                <Grid xs={4} item>
+                    <CustomerSelector />
+                    <Box sx={{marginTop: 2}}>
+                        <FormLabel component="legend">Phone status:</FormLabel>
+                        <Grid container spacing={0}>
+                            {Object.keys(inputs.checkInStat).map((state) => {
+                                return (
+                                    <Grid key={state} item xs={4}>
                                         <FormControlLabel
                                             key={state}
                                             control={
@@ -108,60 +105,16 @@ export default function CreateRepairSlip({ setSnackBarMsg }) {
                                             }
                                             label={state}
                                         />
-                                    )
-                                })}
-                                {/* {() => {
-                                    const formControls = []
-                                    for (const state in inputs.checkInStat) {
-                                        formControls.push(
-                                            <FormControlLabel
-                                                control={
-                                                    <Checkbox checked={inputs.checkInStat[state]} name={state} />
-                                                }
-                                                label={state}
-                                            />
-                                        )
-                                    }
-                                    return formControls
-                                }} */}
-                            </FormGroup>
-                            {/* <FormHelperText>Be careful</FormHelperText>
-                        </FormControl>
-                        <FormControl
-                            required
-                            error={error}
-                            component="fieldset"
-                            sx={{ m: 3 }}
-                            variant="standard"
-                        >
-                            <FormLabel component="legend">Pick two</FormLabel>
-                            <FormGroup>
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox checked={gilad} onChange={handleChange} name="gilad" />
-                                    }
-                                    label="Gilad Gray"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox checked={jason} onChange={handleChange} name="jason" />
-                                    }
-                                    label="Jason Killian"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox checked={antoine} onChange={handleChange} name="antoine" />
-                                    }
-                                    label="Antoine Llorca"
-                                />
-                            </FormGroup>
-                            <FormHelperText>You can display an error</FormHelperText> */}
-                        </FormControl>
+                                    </Grid>
+                                )
+                            })}
+                            <TextField name='notes' label='notes' value={inputs.notes} fullWidth size='small' type='text' />
+                        </Grid>
                     </Box>
                 </Grid>
-                <Grid xs={4} item>
-                    <Box sx={{ height: '100%' }}>
-                        <RepairsToolbar setInputs={setInputs} />
+                <Grid item xs={4}>
+                <Box sx={{ height: '100%' }}>
+                        <RepairsToolbar setInputs={setInputs} inputs={inputs} setSnackBarMsg={setSnackBarMsg} repairsSelection={repairsSelection} setRepairsSelection={setRepairsSelection} />
                         <DataGrid
                             rows={inputs.neededRepairs.map(repair => { return { repair: repair } })}
                             columns={colDef}
@@ -170,12 +123,14 @@ export default function CreateRepairSlip({ setSnackBarMsg }) {
                             density={'compact'}
                             hideFooter
                             disableColumnMenu
+                            onSelectionModelChange={(newSelection)=>{
+                                setRepairsSelection(newSelection)
+                            }}
+                            selectionModel={repairsSelection}
                         />
                     </Box>
                 </Grid>
             </Grid>
-
-            <Button variant='contained' type={'submit'}>Create</Button>
         </Box>
     )
 }
--- src/components/RepairsToolbar.js
+++ src/components/RepairsToolbar.js
@@ -1,21 +1,40 @@
 
-import { Toolbar, TextField, Button } from '@mui/material'
+import { Box, TextField, Button, IconButton } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+
 import { useState } from 'react'
 export default function RepairsToolbar(props) {
-    const [repairValue, setRepairValue] = useState('init')
+    const [repairValue, setRepairValue] = useState('')
     function handleChange(e) {
         setRepairValue(e.target.value)
     }
 
     function handleSubmit() {
-        props.setInputs((oldVal) => { return { ...oldVal, neededRepairs: [...oldVal.neededRepairs, repairValue] } })
+        if (props.inputs.neededRepairs.includes(repairValue)){
+            props.setSnackBarMsg({show: true, message: 'duplicate repair item', severity: 'warning'})
+        } else if (repairValue.length>0){
+            props.setInputs((oldVal) => { return { ...oldVal, neededRepairs: [...oldVal.neededRepairs, repairValue] } })
+            setRepairValue('')
+        }
+
+
     }
     return (
-        <>
-            <TextField size={'small'} type={'text'} name={'repair'} onChange={handleChange} value={repairValue} label={'Add Repairs'}></TextField>
+        <Box sx={{marginBottom: 1}}>
+            <TextField size={'small'} type={'text'} name={'repairItem'} onChange={handleChange} value={repairValue} label={'Add Repairs'}></TextField>
             <Button onClick={handleSubmit}>
                 Add
             </Button>
-            </>
+            {
+                (props.repairsSelection.length>0 && <IconButton sx={{float: 'right'}} onClick={()=>{
+                    props.setInputs((oldVal)=>({...oldVal, neededRepairs: props.inputs.neededRepairs.filter((repairItem)=>{
+                        if (props.repairsSelection.includes(repairItem)){
+                            return false
+                        }
+                        return true
+                    })}))
+                }}><DeleteIcon></DeleteIcon></IconButton>)
+            }
+            </Box>
     )
 }
